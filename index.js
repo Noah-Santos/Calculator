809c72e230cf302cac217f6874f3d3697cdab8ae
NoahSantos <nsanto591@west-mec.org>
2022-11-17 16:22:20 -0700

    Functionality
    Adds more buttons.

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ function change(num){
     document.getElementById("calc").value += num;
 }
 
-//adds a plus sign
-function add(){
-    document.getElementById("calc").value += "+";
-}
-
-//adds a subtraction sign
-function sub(){
-    document.getElementById("calc").value += "-";
-}
-
-//adds a multiplication sign
-function mul(){
-    document.getElementById("calc").value += "*";
-}
-
-//adds a division sign
-function div(){
-    document.getElementById("calc").value += "/";
-}
-
 //calculates the input
 function enter(){
     let equation = document.getElementById("calc").value;
@@ -34,4 +14,38 @@ function enter(){
 //clears the input
 function clearer(){
     document.getElementById("calc").value = " ";
+}
+
+//adds symbol based on the input
+function symbols(num){
+    let input = document.getElementById("calc");
+    if(num == 1){
+        input.value += "(";
+    }else if(num == 2){
+        input.value += ")";
+    }else if(num == 3){
+        input.value += "+";
+    }else if(num == 4){
+        input.value += "-";
+    }else if(num == 5){
+        input.value += "*";
+    }else if(num == 6){
+        input.value += "/";
+    }else if(num == 7){
+        input.value += "%";
+    }else if(num == 8){
+        input.value += "**2";
+    }else if(num == 9){
+        input.value += "**(1/2)";
+    }
+}
+
+//adds the sin, cos, or tan function
+function degree(num){
+    let input = document.getElementById("calc");
+    if(num == 1){
+        input.value += "Math.sin(";
+    }else if(num == 2){
+        
+    }
 }
